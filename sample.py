5a585f07dc8c78c058813021431a6e109c8d051d
unknown <foreigncode@Gmail.com>
2020-02-10 21:40:10 -0500

    Implemented canny edge

--- sample.py
+++ sample.py
@@ -2,48 +2,110 @@ import cv2
 import numpy as np
 import math
 import scipy
+import matplotlib.pyplot as plt
 
-def main():
-    img = cv2.imread("image.png")
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    kernel = 1/16 * np.mat([[1,2,1],[2,4,2],[1,2,1]])
-    #kernel = 1/400 * np.mat ([[1,4,7,4,1],[4,16,26,16,4],[7,26,41,26,7],[4,16,26,16,4],[1,4,7,4,1]])
-    kernel = calc_kernel(0.5)
-    
+im = []
+tit = []
 
-    # sobel_x = np.mat([[2,2,4,2,2],[1,1,2,1,1],[0,0,0,0,0],[-1,-1,-2,-1,-1],[-2,-2,-4,-2,-2]])
-    # sobel_y = np.mat([[2,1,0,-1,-2],[2,1,0,-1,-2],[4,2,0,-2,-4],[2,1,0,-1,-2],[2,1,0,-1,-2]])
+def myEdgeFilter(img0, sigma):
+    hsize = 2 * math.ceil ( 3 * sigma) + 1
+    kernel = calc_kernel(hsize, sigma)
 
+    print("Convolving image with gauss of size %d..." % (hsize))
+    img_smooth = convolve_2d(img0, kernel)
+    im.append(img_smooth)
+    tit.append("Smoothed")
     sobel_x = np.mat([[1,2,1],[0,0,0],[-1,-2,-1]])
     sobel_y = np.mat([[-1,0,1],[-2,0,2],[-1,0,1]])
+    print("Convolving image with x sobel...")
+    img_x = convolve_2d(img_smooth, sobel_x)
+    im.append(img_x)
+    tit.append("X Sobel")
+    print("Convolving image with y sobel...")
+    img_y = convolve_2d(img_smooth, sobel_y)
+    im.append(img_y)
+    tit.append("Y Sobel")
 
-    img2 = cross_correlation_2d(img,kernel)
-    
-    xsobel = cross_correlation_2d(img2,sobel_x)
-    img3 = cross_correlation_2d(img2,sobel_y)
-    grad, dirMat = gradient(xsobel, img3, 25)
+    print("Finding gradient dir and magnitude...")
+    gradMag, gradDir = gradient(img_x, img_y, 50)
+    im.append(gradMag)
+    tit.append("Gradient")
+    print("Performing non-maxima suppression...")
+    img_non_max = non_max_suppression(gradMag, gradDir)
+    im.append(img_non_max)
+    tit.append("Non-maxima")
+    thresh_calc(img_non_max,100,200)
+    return img_non_max
 
-    img2 = cv2.convertScaleAbs(img2)
 
-    imgNone = non_max_suppression(grad, dirMat)
-    
-    
 
-    grad = cv2.convertScaleAbs(grad)
-    xsobel = cv2.convertScaleAbs(xsobel)
-    img3 = cv2.convertScaleAbs(img3)
-    imgNone = cv2.convertScaleAbs(imgNone)
-    
-    cv2.imshow("smooth",img2)
-    cv2.imshow("xsobel",xsobel)
-#     cv2.imshow("ysobelcv",cv2Canny)
-    cv2.imshow("ysobel",img3)
-    cv2.imshow("gradient",grad)
-    cv2.imshow("non-max",imgNone)
-    cv2.imshow("gradOr",dirMat)
+def main():
+    img_cat = cv2.imread("cat.png",cv2.IMREAD_GRAYSCALE)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_dog = cv2.imread("dog.png",cv2.IMREAD_GRAYSCALE)
+  
+
+    khp = np.mat([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
+    # canny = myEdgeFilter(img, 0.25) 
+    # im.append(canny)
+    # tit.append("Canny")
+    gauss_3x3 = calc_kernel(3, 0.25)
+
+    fil = high_pass(img_cat, calc_kernel(51,5))
+    im.append(fil)
+    tit.append("high pass")
+
+    fil2 = low_pass(img_dog, gauss_3x3)
+    im.append(fil2)
+    tit.append("low pass")
+
+    hybrid = fil + fil2
+    im.append(hybrid)
+    tit.append("hybrid")
+    # cv2.imshow("hipghas",hp)
+    # cv2.imshow("im2",img2)
+    # cv2.imshow("image",img)
+
+    # plt.show()
+    show_images(im,titles=tit)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+def high_pass(img, kernel):
+    smooth_img = convolve_2d(img, kernel)
+    fil = img - smooth_img
+    return fil
+    
+def low_pass(img, kernel):
+    return convolve_2d(img, kernel)
+
+
+def show_images(images, cols = 1, titles = None):
+    #https://gist.github.com/soply/f3eec2e79c165e39c9d540e916142ae1
+    """Display a list of images in a single figure with matplotlib.
+   
+    Parameters
+    ---------
+    images: List of np.arrays compatible with plt.imshow.
+   
+    cols (Default = 1): Number of columns in figure (number of rows is
+                        set to np.ceil(n_images/float(cols))).
+   
+    titles: List of titles corresponding to each image. Must have
+            the same length as titles.
+    """
+    assert((titles is None)or (len(images) == len(titles)))
+    n_images = len(images)
+    if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
+    fig = plt.figure()
+    for n, (image, title) in enumerate(zip(images, titles)):
+        a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
+        # if image.ndim == 2:
+        #     plt.gray()
+        plt.imshow(image, cmap='gray')
+        a.set_title(title)
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images + 1000)
+    plt.show()
 def cross_correlation_2d(image, kernel):
     m, n = kernel.shape
     if (m == n):
@@ -65,6 +127,7 @@ def cross_correlation_2d(image, kernel):
         return np.mat(result)
     return [[]]
 
+
 def convolve_2d(image, kernel):
     kernel = rotate_matrix_180(kernel)
     return cross_correlation_2d(image, kernel)
@@ -81,6 +144,16 @@ def rotate_matrix_180(kernel):
             h += 1
             k -= 1
     return new_ker
+def thresh_calc(img, t1, t2):
+    y, x = img.shape
+    for i in range(y):
+        for j in range(x):
+            if img[i,j] < t1:
+                img[i,j] = 0
+            elif img[i,j] >= t1 and img[i,j] < t2:
+                img[i,j] = t1
+            else:
+                img[i,j] = t2
 
 def non_max_suppression(grad, dirmat):
     y, x = grad.shape
@@ -127,9 +200,9 @@ def gradient(imagex, imagey, threshold):
                 else:
                     new_img[i,j] = 0
         ori = np.degrees(np.arctan2(imagex,imagey))
+        new_img = new_img * (255 / new_img.max())
         return new_img, ori
-def calc_kernel(sigma):
-    hsize = 2 * math.ceil(3 * sigma) + 1
+def calc_kernel(hsize, sigma):
     kernel = np.zeros((hsize,hsize))
     offset = math.floor(hsize/2)
     sum = 0
